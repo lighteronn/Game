014b2f58cd04b818cb0ddd96ece2ea27fb5c6353
lighteronn <mezora197@gmail.com>
2020-11-04 21:15:52 +0100

    completed

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-let records = [];
-let userInput;
+//Record the numbers created by the computer
 
+$(document).ready(() => {
 
- $(document).ready(() => {
-
+  //Activate the modal and add event listener
   $("#myModal").modal('show');
-
-  $('.btn').on('click', event => {
+  $('.btn-level').on('click', event => {
+    //Deactivate the modal
     $("#myModal").modal('hide');
-    userInput = event.target.parentNode.value.value
-    console.log(userInput)
+    //Get the user's level input 
+    let level = event.target.parentNode.value.value
+    console.log(level, " level");
   
   
 
-     // stops modal from being shown
-     
-     
-     
-  
- 
-
-//var level = prompt('Select the level') 
-
-  
-
-
-if(userInput) {
+if(level) {
+//Create cards and record the numbers generated with records array
+let records = [];
 document.querySelector('.card-container').innerHTML = '';
-for(let i =0; i < userInput; i++ ) {
+for(let i =0; i < level; i++ ) {
     let number = Math.floor(Math.random()*6+6);
     records.push(number)
     document.querySelector('.card-container').innerHTML += `<div class="card text-center flip-card" style="width: 15%; height:12rem;">
@@ -42,41 +32,58 @@ for(let i =0; i < userInput; i++ ) {
   </div>`
 };
 
+//Flip the cards by 180 degree
  document.querySelectorAll('.card').forEach( (card,index) => {
     console.log(index)
     setTimeout(() => {card.classList.toggle('flip-card-flipper')}, 1000*(index));
  })
  
+ //Make the numbers invisible with '?'
 setTimeout(() => {
     document.querySelectorAll('.card-text').forEach(text => { 
         text.textContent = '?';
-        })
-}, 1000* records.length-100)
+    })}, 1000* records.length-100)
+
 console.log(records, records.join())
+
 setTimeout(() => {
-     userInput = prompt('Enter numbers')
-     if(userInput.split(',').join('') == records.join('') || userInput.trim() == records.join('') ) {
-      alert("You won")
-     } else {
-         alert("You lost the game")
-         
-     }
-     let restart = confirm('Do you want to continue?') 
-    if(restart) {
-      location.reload();
+    
+    //Activate the modal
+    $('#myModal-input').modal('show');
+    //Add event listener to the modal
+    $('.btn-input').on('click', event => {
+      //Deactivate the modal
+      $('#myModal-input').modal('hide');
+      //User enters the number
+      let userInput = event.target.parentNode.value.value
+      console.log(userInput, " userInput")
+      
+      if(userInput.split(',').join('') == records.join('') || userInput == records.join(' ')) {
+           //User won the game and restart modal is shown
+          document.querySelector('#myModal-restart .modal-title').textContent = 'You Won';
+           $('#myModal-restart').modal('show');
+           $('.btn-restart').on('click', () => { 
+             $('#myModal-restart').modal('hide') 
+             location.reload()
+           })
+        //User lose the game and restart modal is shown 
+       } else {
+        document.querySelector('#myModal-restart .modal-title').textContent = 'You Lost!';
+        $('#myModal-restart').modal('show');
+        $('.btn-restart').on('click', (e) => { 
+          $('#myModal-restart').modal('hide')
+          location.reload()
+          } )
+        
+
     }
-     
-    }, 1000*records.length);
+  })
   
-}
+     
+   }, 1000*records.length);
+  
+  }
 
 
 });
 })
-
-
-
-let restart = confirm('Do you want to continue?') 
-    if(restart) {
-      location.reload();
-    }
